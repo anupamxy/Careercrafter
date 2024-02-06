6402622c749816aea9c890a4152797001317b498
anupamxy <123785384+anupamxy@users.noreply.github.com>
2024-02-07 02:11:39 +0530

    welcome and homepage

---
+++ careerclient/src/pages/Welcomepage.js
@@ -0,0 +1,18 @@
+
+
+import React from 'react'
+import Layout from './../components/Layouts/Layout';
+
+const Welcomepage = () => {
+  return (
+<Layout>
+    <div className='welcome'>
+    <h1  ></h1>
+    </div>
+    </Layout>
+   
+   
+  )
+}
+
+export default Welcomepage
